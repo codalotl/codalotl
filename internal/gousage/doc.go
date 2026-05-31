fe32f1a74af0110453b3233e38b100aba596b618
Jonathan Novak <jonathan.e.novak@gmail.com>
2026-05-30 17:55:37 -0700

    Add gousage documentation

---
+++ internal/gousage/doc.go
@@ -0,0 +1,4 @@
+// Package gousage finds packages in a Go module that import a target package.
+//
+// Use UsedBy to list the packages in the target package's module that import it; each result includes the using package's import path and filesystem location.
+package gousage
